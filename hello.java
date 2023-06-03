ac027c24e5743981d5a9f8ed0b8fa9adba24d468
CSA7612 <134828710+CSA7612@users.noreply.github.com>
2023-06-03 16:39:39 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@
 class HelloWorld {
     public static void main(String[] args) 
         System.out.println("Hello, india!");
-        System.out.println("Hello, Wor!");
+        System.out.println("Hello");
 		System.out.println("Hello, poooori"); 
 	    		 
     }
