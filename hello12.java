bbb93cef93572bab8a000f3ae52f7fd617698c77
csa7612 <csa7612@gmail.com>
2023-06-03 16:21:06 +0530

    hello12 file released

---
+++ hello12.java
@@ -0,0 +1,10 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) 
+        System.out.println("Hello, india!");
+        System.out.println("Hello, Wor!");
+		System.out.println("Hello,"); 
+	    		 
+    }
+}
